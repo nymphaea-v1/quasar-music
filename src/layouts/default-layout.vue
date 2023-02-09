1370b73ec835d869acb9a4c2a0b2eecd0da04df6
nymphaea-v1 <roselin.6@yandex.ru>
2023-02-09 20:17:05 +0500

    add default layout

--- src/layouts/default-layout.vue
+++ src/layouts/default-layout.vue
@@ -1,11 +1,65 @@
 <template>
-  <div>
-    {{ w }}
-    <router-view />
-  </div>
+  <q-layout view="hHh lpr fff">
+    <q-header
+      elevated
+      class="bg-blue-grey-9"
+    >
+      <q-toolbar>
+        <q-toolbar-title
+          shrink
+          class="text-weight-bold no-pointer-events"
+        >
+          Music Library
+        </q-toolbar-title>
+        <q-tabs>
+          <q-route-tab
+            to="/albums"
+            label="Albums"
+          />
+          <q-route-tab
+            to="/artists"
+            label="Artists"
+          />
+          <q-route-tab
+            to="/songs"
+            label="Songs"
+          />
+        </q-tabs>
+      </q-toolbar>
+    </q-header>
+
+    <q-page-container>
+      <router-view />
+    </q-page-container>
+
+    <q-footer
+
+      class="bg-blue-grey-10 footer q-pa-md"
+    >
+      <div>
+        created by
+        <a
+          href="https://github.com/nymphaea-v1"
+          class="author-link cursor-pointer"
+          target="_blank"
+        >nymphaea-v1</a>
+      </div>
+    </q-footer>
+  </q-layout>
 </template>
 
 <script setup lang="ts">
-import { ref } from 'vue'
-const w = ref(1)
 </script>
+
+<style scoped lang="scss">
+.author-link {
+  color: white;
+  font-weight: 700;
+  text-decoration: none;
+
+  &:hover {
+    color: $primary;
+  }
+}
+
+</style>
